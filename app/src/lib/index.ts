daeaf7fd606ff5619dec8ca65c99f503dba16cdd
Suvesh Pandey <jpsuvesh29@gmail.com>
2025-09-20 23:22:02 +0530

    fixed libs

--- app/src/lib/index.ts
+++ app/src/lib/index.ts
@@ -1,15 +1,13 @@
 import project1 from "../assets/projectsImg/tailkify-img.png";
 import project2 from "../assets/projectsImg/gemini-clone-img.png";
 import project3 from "../assets/projectsImg/neura-notes-img.png";
-import project4 from "../assets/projectsImg/voting-app-img.png";
-import project5 from "../assets/projectsImg/tic-tac-toe-img.png";
-import project6 from "../assets/projectsImg/excaliboard-dashboard.png";
-import project7 from "../assets/projectsImg/code-pilot.png";
+import project4 from "../assets/projectsImg/excaliboard-dashboard.png";
+import project5 from "../assets/projectsImg/code-pilot.png";
 
 export const PROJECTS = [
     {
         title: "CodePilot – AI-Assisted Code Learning Platform",
-        image: project7,
+        image: project5,
         description: "CodePilot is an AI-assisted code learning platform built with Next.js, TypeScript, Tailwind CSS, and PostgreSQL. It provides a seamless environment where users can write and run code in multiple languages while receiving real-time AI-powered hints, one-click code fixes, debugging support, and chatbot assistance for better concept clarity. Alongside the code editor, the platform features personal notes, blogs, and an AI-powered quiz for interactive learning. It also integrates YouTube videos with infinite scrolling and caching for smooth performance. With secure authentication via email OTP and Google OAuth, CodePilot offers an engaging, efficient, and accessible way for students and beginners to learn and practice coding.",
         technologies: ["Next.js", "TypeScript", "Tailwind CSS", "PostgreSQL", "NextAuth.js", "shadcn/ui", "Vercel (Deployment)", "Google Gemini", "Intersection Observer API (infinite scroll)", "Node-Cache"],
         live: "https://code-pilot-kappa.vercel.app/",
@@ -17,7 +15,7 @@ export const PROJECTS = [
     },
     {
         title: "Excaliboard - Real-time collaborative canvas app (excalidraw.com clone)",
-        image: project6,
+        image: project4,
         description: "Excalidraw.com-inspired real-time collaborative canvas built with Next.js, WebSockets, and PostgreSQL. Supports multi-user drawing, Google Auth, live room dashboards, and persistent canvas storage. Built in a scalable monorepo with a responsive UI using Tailwind CSS.",
         technologies: ["NextJS", "PostgreSQL", "NodeJS", "ExpressJS", "Typescript", "Tailwind", "WebSocket (ws)", "NextAuth.js", "Monorepo Structure"],
         live: "https://excaliboard-frontend.vercel.app/",
